edae47cf9e0a31bb4f24391ce446e03054416cc8
kekee000 <kekee000@gmail.com>
2025-11-18 11:12:01 +0800

    add data proxy api cases

---
+++ src/data-proxy-api-component/component.js
@@ -0,0 +1,26 @@
+// update text
+var san = require('san');
+class MyComponent extends san.Component{
+    static template = '<div>{{count|formatCount}} list: <span s-for="name in list">{{name}}</span></div>';
+    static computed = {
+        newName() {
+            return this.d.name + ' - icon';
+        }
+    }
+    static filters = {
+        formatCount(count) {
+            return this.d.newName + ': ' + count;
+        }
+    }
+
+    inited () {
+        this.d.count = 2;
+        this.initList();
+    }
+
+    initList() {
+        this.d.list.push(this.d.newName);
+    }
+}
+
+exports = module.exports = MyComponent;
---
+++ src/data-proxy-api-component/component.ts
@@ -0,0 +1,28 @@
+// update text
+import san from 'san';
+
+class MyComponent extends san.Component<any> {
+    static template = '<div>{{count|formatCount}} list: <span s-for="name in list">{{name}}</span></div>';
+    static computed = {
+        newName(this: MyComponent) {
+            return this.d.name + ' - icon';
+        }
+    }
+
+    static filters = {
+        formatCount(this: MyComponent, count: string) {
+            return this.d.newName + ': ' + count;
+        }
+    }
+
+    inited () {
+        this.d.count = 2;
+        this['initList']();
+    }
+
+    initList() {
+        this.d.list.push(this.d.newName);
+    }
+}
+
+export default MyComponent;
